bae806e60761be5c4bdd3a611a016bf1204bfb43
Pratik Agarwal <pratikkyal27@gmail.com>
2024-11-22 00:59:55 -0800

    Update views.py

--- apple/views.py
+++ apple/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def myfunc(request):
-    return HttpResponse("my name is Manasvi")
+    return HttpResponse("Hi This is Pratik")
